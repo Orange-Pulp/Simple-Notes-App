b213ffe47dc4f897d10f5ab24263af1aa1f476fb
Paul Page <48072018+TwentyFirstCenturyBean@users.noreply.github.com>
2019-09-04 22:01:29 -0800

    Update Word-Processer.py

--- Word-Processer.py
+++ Word-Processer.py
@@ -1,10 +1,24 @@
-Textinput = input(">>>")
+#Some instruction
+print("After writing Enter (savefile) to save text to a new file")
+print("Type (save) to save to that existing file, or type (exit) to exit")
+print("")
+#Text saving portion
+Writing = True
 
-data=[Textinput]
+while Writing:
+    Textinput = input(">>>")
+    data=[Textinput]
 
-try:
-    with open("Save-data", "w") as out:
-        out.write('\n'.join(data))
-        print("Writing saved")
-except:
-     print("Unable to save data to your computer")
+    if "(savefile)" in Textinput:
+            with open("Writting", "w") as out_file:
+                out_file.write('\n'.join(data))
+                print("Writing saved")
+
+    if "(save)" in Textinput:
+            with open("Writting", "w") as out_file:
+                out_file.write('\n'.join(data))
+                print("Writing saved")
+        
+    if "(exit)" in Textinput:
+        print("Exiting program")
+        Writing = False
