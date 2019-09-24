cc7da0bf2fc203d10e1719533449ac67753c1fc5
Paul Page <48072018+TwentyFirstCenturyBean@users.noreply.github.com>
2019-09-23 22:41:49 -0800

    Update Word Processer.py

--- Word Processer.py
+++ Word Processer.py
@@ -1,27 +1,27 @@
+#This thing is used for checking directories and some other stuff
 import os.path
 
 print("")
 print("After writing enter (save) in parenthisis to save your writting to a text file")
 print("Type (save) again to add to that existing file, or type (exit) to exit")
 print("")
-#Text writting snd saving portion
+
 Writing = True
 
 while Writing:
-    Textinput = input("")
+    Textinput = input(">")
     data=[Textinput]
-
+#Some fancy stuff for writting text to files amoung other stuff 
     if "(save)" in Textinput:
         Save_text = Textinput.replace("(save)", "")
-        with open("Writting_file.txt", "w") as out_file:
-            out_file.write(Save_text)
-            print("Writing saved")
-            
-
-    if "(save)" in Textinput and os.path.exists("Writting_file.txt"):
-        Save_text = Textinput.replace("(save)", "")
-        with open("Writting_file.txt", "a") as out_file:
-            out_file.write(Save_text)
+        if os.path.exists("Writting file.txt"): #This is checking if we already saved text to a file. If so we will continue to use that one insdead of creating another 
+            with open("Writting file.txt", "a") as file:
+                file.write(Save_text)
+                print("Writting saved")
+        else:
+            with open("Writting file.txt", "w") as file:
+                file.write(Save_text)
+                print("Writing saved")
 
     if "(exit)" in Textinput:
         Writting = False 
